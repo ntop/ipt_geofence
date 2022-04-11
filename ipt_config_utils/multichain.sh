d09f0c70fcc5fc187c42beef3a61fb645b9ed20c
Luca Deri <deri@ntop.org>
2022-04-11 09:28:19 +0200

    Added IPv6 support to multichain.sh

--- ipt_config_utils/multichain.sh
+++ ipt_config_utils/multichain.sh
@@ -36,67 +36,74 @@ conntrack -U --mark 0 > /dev/null 2>&1
 # Routing needed by DNAT (DNS)
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Flush chains (if present)
-iptables -t mangle -F GEO_PREROUTING
-iptables -t mangle -F GEO_POSTROUTING
-iptables -t mangle -F GEO_OUTPUT
-iptables -t mangle -X GEO_PREROUTING
-iptables -t mangle -X GEO_POSTROUTING
-iptables -t mangle -X GEO_OUTPUT
+IPTABLES="iptables"
+# We will execute this code using "ip6tables" instead of "iptables" the second time
+for i in {1,2}; do
+    # Flush chains (if present)
+    $IPTABLES -t mangle -F GEO_PREROUTING
+    $IPTABLES -t mangle -F GEO_POSTROUTING
+    $IPTABLES -t mangle -F GEO_OUTPUT
+    $IPTABLES -t mangle -X GEO_PREROUTING
+    $IPTABLES -t mangle -X GEO_POSTROUTING
+    $IPTABLES -t mangle -X GEO_OUTPUT
 
-# Create chains
-iptables -t mangle -N GEO_PREROUTING
-iptables -t mangle -N GEO_POSTROUTING
-iptables -t mangle -N GEO_OUTPUT
+    # Create chains
+    $IPTABLES -t mangle -N GEO_PREROUTING
+    $IPTABLES -t mangle -N GEO_POSTROUTING
+    $IPTABLES -t mangle -N GEO_OUTPUT
 
-# Read CONNMARK and set it in mark
-# (A) For incoming packets
-iptables -t mangle -A GEO_PREROUTING -j CONNMARK --restore-mark
+    # Read CONNMARK and set it in mark
+    # (A) For incoming packets
+    $IPTABLES -t mangle -A GEO_PREROUTING -j CONNMARK --restore-mark
 
-# (B) For locally generated packets
-iptables -t mangle -A GEO_OUTPUT -j CONNMARK --restore-mark
+    # (B) For locally generated packets
+    $IPTABLES -t mangle -A GEO_OUTPUT -j CONNMARK --restore-mark
 
-# Save CONNMARK (1st rule of POSTROUTING)
-iptables -t mangle -A GEO_POSTROUTING -j CONNMARK --save-mark
+    # Save CONNMARK (1st rule of POSTROUTING)
+    $IPTABLES -t mangle -A GEO_POSTROUTING -j CONNMARK --save-mark
 
-# PASS (1)
-iptables -t mangle -A GEO_PREROUTING --match mark --mark 1 -j ACCEPT
+    # PASS (1)
+    $IPTABLES -t mangle -A GEO_PREROUTING --match mark --mark 1 -j ACCEPT
 
-# DROP (2)
-iptables -t mangle -A GEO_PREROUTING --match mark --mark 2 -j DROP
+    # DROP (2)
+    $IPTABLES -t mangle -A GEO_PREROUTING --match mark --mark 2 -j DROP
 
-# Send traffic to NFQUEUE
-iptables -t mangle -A GEO_PREROUTING  -p tcp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
-iptables -t mangle -A GEO_OUTPUT      -p tcp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
+    # Send traffic to NFQUEUE
+    $IPTABLES -t mangle -A GEO_PREROUTING  -p tcp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
+    $IPTABLES -t mangle -A GEO_OUTPUT      -p tcp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
 
-iptables -t mangle -A GEO_PREROUTING  -p udp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
-iptables -t mangle -A GEO_OUTPUT      -p udp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
+    $IPTABLES -t mangle -A GEO_PREROUTING  -p udp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
+    $IPTABLES -t mangle -A GEO_OUTPUT      -p udp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
 
-iptables -t mangle -A GEO_PREROUTING  -p icmp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
-iptables -t mangle -A GEO_OUTPUT      -p icmp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
+    $IPTABLES -t mangle -A GEO_PREROUTING  -p icmp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
+    $IPTABLES -t mangle -A GEO_OUTPUT      -p icmp --match mark --mark 0 -j NFQUEUE --queue-num $QUEUE_ID --queue-bypass
 
-###################################################
+    ###################################################
 
-# Add final return
-iptables -t mangle -A GEO_PREROUTING  -j RETURN
-iptables -t mangle -A GEO_POSTROUTING -j RETURN
-iptables -t mangle -A GEO_OUTPUT      -j RETURN
+    # Add final return
+    $IPTABLES -t mangle -A GEO_PREROUTING  -j RETURN
+    $IPTABLES -t mangle -A GEO_POSTROUTING -j RETURN
+    $IPTABLES -t mangle -A GEO_OUTPUT      -j RETURN
 
-###################################################
+    ###################################################
 
-# Append chains
-iptables -t mangle -A PREROUTING  -j GEO_PREROUTING
-iptables -t mangle -A POSTROUTING -j GEO_POSTROUTING
-iptables -t mangle -A OUTPUT      -j GEO_OUTPUT
+    # Append chains
+    $IPTABLES -t mangle -A PREROUTING  -j GEO_PREROUTING
+    $IPTABLES -t mangle -A POSTROUTING -j GEO_POSTROUTING
+    $IPTABLES -t mangle -A OUTPUT      -j GEO_OUTPUT
 
-###################################################
+    ###################################################
 
-# Show all
-iptables -nvL -t mangle
+    # Show all
+    $IPTABLES -nvL -t mangle
 
-iptables -t mangle -L GEO_PREROUTING
-iptables -t mangle -L GEO_POSTROUTING
-iptables -t mangle -L GEO_OUTPUT
+    $IPTABLES -t mangle -L GEO_PREROUTING
+    $IPTABLES -t mangle -L GEO_POSTROUTING
+    $IPTABLES -t mangle -L GEO_OUTPUT
+
+    # Same code but using IPv6
+    IPTABLES="ip6tables"
+done
 
 # Flush conntrack
 #conntrack -F
